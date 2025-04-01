f6fc4a9be3a7e65807b7bd7aae22531b9c32a807
Benjamin D Howell <howellbe@bc.edu>
2025-04-01 17:17:55 -0400

    Upload

---
+++ MMA ML Project/SkillEstimates.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+script_dir = Path(__file__).resolve().parent
+
+data = pd.read_excel(script_dir / "data" / "MMAMatches.xlsx")
+# Drop rows where Weight Class (lbs) is NaN
+data = data.dropna(subset=["Weight Class (lbs)"])
+data = data.iloc[:100]
